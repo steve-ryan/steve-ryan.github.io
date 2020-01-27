2026777cae8fbf8d80c7bcb7d3cd16843ebf0794
stephen wachira <stevohyoung@gmail.com>
2020-01-27 22:24:15 +0300

    converted to react website

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Footer extends React.Component{
+    render(){
+        return (
+          <p>
+            Copyright &copy; <span>2020 </span>
+            <a href="https://github.com/steve-ryan">Steve Ryan</a>
+          </p>
+        );
+    }
+}
+export default Footer;
---
+++ src/Social.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+class Social extends React.Component{
+    render(){
+        return (
+          <div class="icons-social">
+            <a href="https://github.com/steve-ryan">
+              <i class="fab fa-github"></i>
+            </a>
+            <a  href="https://twitter.com/Stevewachirah">
+              <i class="fab fa-twitter"></i>
+            </a>
+            <a  href="https://dev.to/steveryan">
+              <i class="fab fa-dev"></i>
+            </a>
+            <a
+              
+              href="https://www.linkedin.com/in/steve-wachirah/"
+            >
+              <i class="fab fa-linkedin"></i>
+            </a>
+            <a  href="https://medium.com/@steveryan_50665">
+              <i class="fab fa-medium"></i>
+            </a>
+            <a  href="https://codepen.io/Steve_ryan">
+              <i class="fab fa-codepen"></i>
+            </a>
+          </div>
+        );
+    }
+}
+
+export default Social;
